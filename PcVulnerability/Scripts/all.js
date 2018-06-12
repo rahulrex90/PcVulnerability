9612770258d6a4f887aa2e5b239aefc6c6b3e961
Rahul Kumar Sharma <rahulrex90@gmail.com>
2018-06-12 22:57:04 +0530

    Add Project

---
+++ PcVulnerability/Scripts/all.js
@@ -0,0 +1,423 @@
+var All = function () {
+
+    var handleDataTables = function() {
+        if (jQuery().dataTable) {
+            var table = $('#make-data-table');
+
+            // begin first table
+            table.dataTable({
+
+                // Internationalisation. For more info refer to http://datatables.net/manual/i18n
+                "language": {
+                    "aria": {
+                        "sortAscending": ": activate to sort column ascending",
+                        "sortDescending": ": activate to sort column descending"
+                    },
+                    "emptyTable": "No data available in table",
+                    "info": "Showing _START_ to _END_ of _TOTAL_ records",
+                    "infoEmpty": "No records found",
+                    "infoFiltered": "(filtered1 from _MAX_ total records)",
+                    "lengthMenu": "Show _MENU_",
+                    "search": "Search:",
+                    "zeroRecords": "No matching records found",
+                    "paginate": {
+                        "previous":"Prev",
+                        "next": "Next",
+                        "last": "Last",
+                        "first": "First"
+                    }
+                },
+
+                // Or you can use remote translation file
+                //"language": {
+                //   url: '//cdn.datatables.net/plug-ins/3cfcc339e89/i18n/Portuguese.json'
+                //},
+
+                // Uncomment below line("dom" parameter) to fix the dropdown overflow issue in the datatable cells. The default datatable layout
+                // setup uses scrollable div(table-scrollable) with overflow:auto to enable vertical scroll(see: assets/global/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.js). 
+                // So when dropdowns used the scrollable div should be removed. 
+                //"dom": "<'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r>t<'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>",
+
+                "bSort" : false,
+                "bStateSave": true, // save datatable state(pagination, sort, etc) in cookie.
+                "lengthMenu": [
+                    [5, 15, 20, -1],
+                    [5, 15, 20, "All"] // change per page values here
+                ],
+                // set the initial value
+                "pageLength": 15,            
+                "pagingType": "bootstrap_full_number",
+                "columnDefs": [{  // set default column settings
+                    'orderable': false,
+                    'targets': [-1]
+                }, {
+                    "searchable": false,
+                    "targets": [-1]
+                }]
+            });
+            
+            jQuery('#make-data-table_wrapper .dataTables_filter input').addClass("form-control input-small input-inline"); // modify table search input
+            jQuery('#make-data-table_wrapper .dataTables_length select').addClass("form-control input-xsmall"); // modify table per page dropdown
+            jQuery('#make-data-table_wrapper .dataTables_length select').select2(); // initialize select2 dropdown
+        }
+    }
+    
+    var handleCheckableTable = function () {
+
+        if (jQuery().dataTable) {
+            var table = $('#checkable-data-table');
+
+            // begin first table
+            table.dataTable({
+
+                // Internationalisation. For more info refer to http://datatables.net/manual/i18n
+                "language": {
+                    "aria": {
+                        "sortAscending": ": activate to sort column ascending",
+                        "sortDescending": ": activate to sort column descending"
+                    },
+                    "emptyTable": "No data available in table",
+                    "info": "Showing _START_ to _END_ of _TOTAL_ records",
+                    "infoEmpty": "No records found",
+                    "infoFiltered": "(filtered1 from _MAX_ total records)",
+                    "lengthMenu": "Show _MENU_",
+                    "search": "Search:",
+                    "zeroRecords": "No matching records found",
+                    "paginate": {
+                        "previous":"Prev",
+                        "next": "Next",
+                        "last": "Last",
+                        "first": "First"
+                    }
+                },
+
+                // Or you can use remote translation file
+                //"language": {
+                //   url: '//cdn.datatables.net/plug-ins/3cfcc339e89/i18n/Portuguese.json'
+                //},
+
+                // Uncomment below line("dom" parameter) to fix the dropdown overflow issue in the datatable cells. The default datatable layout
+                // setup uses scrollable div(table-scrollable) with overflow:auto to enable vertical scroll(see: assets/global/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.js). 
+                // So when dropdowns used the scrollable div should be removed. 
+                //"dom": "<'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r>t<'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>",
+
+                "bStateSave": true, // save datatable state(pagination, sort, etc) in cookie.
+                "bSort" : false,
+                "columnDefs": [ {
+                    "targets": 0,
+                    "orderable": false,
+                    "searchable": false
+                }],
+
+                "lengthMenu": [
+                    [5, 15, 20, -1],
+                    [5, 15, 20, "All"] // change per page values here
+                ],
+                // set the initial value
+                "pageLength": 15,            
+                "pagingType": "bootstrap_full_number",
+                "columnDefs": [{  // set default column settings
+                    'orderable': false,
+                    'targets': [0]
+                }, {
+                    "searchable": false,
+                    "targets": [0]
+                }]
+            });
+
+            var tableWrapper = jQuery('#checkable-data-table_wrapper');
+
+            table.find('.group-checkable').change(function () {
+                var set = jQuery(this).attr("data-set");
+                var checked = jQuery(this).is(":checked");
+                jQuery(set).each(function () {
+                    if (checked) {
+                        $(this).prop("checked", true);
+                        $(this).parents('tr').addClass("active");
+                    } else {
+                        $(this).prop("checked", false);
+                        $(this).parents('tr').removeClass("active");
+                    }
+                });
+                jQuery.uniform.update(set);
+            });
+
+            table.on('change', 'tbody tr .checkboxes', function () {
+                $(this).parents('tr').toggleClass("active");
+            });
+        }
+    }
+
+    // advance validation
+    var handleValidation = function() {
+        // for more info visit the official plugin documentation: 
+        // http://docs.jquery.com/Plugins/Validation
+
+            var form3 = $('.validate-form');
+            var error3 = $('.alert-danger', form3);
+            var success3 = $('.alert-success', form3);
+
+            form3.validate({
+                ignore: '.tt-hint, :hidden',
+                errorElement: 'span', //default input error message container
+                errorClass: 'help-block help-block-error', // default input error message class
+                focusInvalid: false, // do not focus the last invalid input
+                rules: {
+                    name: {
+                        minlength: 2,
+                        required: true
+                    },
+                    email: {
+                        required: true,
+                        email: true
+                    }
+                },
+
+                errorPlacement: function (error, element) { // render error placement for each input type
+                    if (element.parent(".input-group").size() > 0) {
+                        error.insertAfter(element.parent(".input-group"));
+                    } else if (element.attr("data-error-container")) { 
+                        error.appendTo(element.attr("data-error-container"));
+                    } else if (element.parents('.radio-list').size() > 0) { 
+                        error.appendTo(element.parents('.radio-list').attr("data-error-container"));
+                    } else if (element.parents('.radio-inline').size() > 0) { 
+                        error.appendTo(element.parents('.radio-inline').attr("data-error-container"));
+                    } else if (element.parents('.checkbox-list').size() > 0) {
+                        error.appendTo(element.parents('.checkbox-list').attr("data-error-container"));
+                    } else if (element.parents('.checkbox-inline').size() > 0) { 
+                        error.appendTo(element.parents('.checkbox-inline').attr("data-error-container"));
+                    } else {
+                        error.insertAfter(element); // for other inputs, just perform default behavior
+                    }
+                },
+
+                invalidHandler: function (event, validator) { //display error alert on form submit   
+                    success3.hide();
+                    error3.show();
+                    App.scrollTo(error3, -200);
+                },
+
+                highlight: function (element) { // hightlight error inputs
+                   $(element)
+                        .closest('.form-group').addClass('has-error'); // set error class to the control group
+                },
+
+                unhighlight: function (element) { // revert the change done by hightlight
+                    $(element)
+                        .closest('.form-group').removeClass('has-error'); // set error class to the control group
+                },
+
+                success: function (label) {
+                    label
+                        .closest('.form-group').removeClass('has-error'); // set success class to the control group
+                }
+            });
+
+             //apply validation on select2 dropdown value change, this only needed for chosen dropdown integration.
+            $('.select2me', form3).change(function () {
+                form3.validate().element($(this)); //revalidate the chosen dropdown value and show error or success message for the input
+            });
+            
+            $('.validate-form .date-picker .form-control', form3).change(function() {
+                form3.validate().element($(this)); //revalidate the chosen dropdown value and show error or success message for the input 
+            })
+    }
+    
+    var handleValidation2 = function() {
+        // for more info visit the official plugin documentation: 
+        // http://docs.jquery.com/Plugins/Validation
+
+            var form3 = $('.validate-form2');
+            var error3 = $('.alert-danger', form3);
+            var success3 = $('.alert-success', form3);
+
+            form3.validate({
+                errorElement: 'span', //default input error message container
+                errorClass: 'help-block help-block-error', // default input error message class
+                focusInvalid: false, // do not focus the last invalid input
+                ignore: "", // validate all fields including form hidden input
+                rules: {
+                    name: {
+                        minlength: 2,
+                        required: true
+                    },
+                    email: {
+                        required: true,
+                        email: true
+                    }
+                },
+
+                errorPlacement: function (error, element) { // render error placement for each input type
+                    if (element.parent(".input-group").size() > 0) {
+                        error.insertAfter(element.parent(".input-group"));
+                    } else if (element.attr("data-error-container")) { 
+                        error.appendTo(element.attr("data-error-container"));
+                    } else if (element.parents('.radio-list').size() > 0) { 
+                        error.appendTo(element.parents('.radio-list').attr("data-error-container"));
+                    } else if (element.parents('.radio-inline').size() > 0) { 
+                        error.appendTo(element.parents('.radio-inline').attr("data-error-container"));
+                    } else if (element.parents('.checkbox-list').size() > 0) {
+                        error.appendTo(element.parents('.checkbox-list').attr("data-error-container"));
+                    } else if (element.parents('.checkbox-inline').size() > 0) { 
+                        error.appendTo(element.parents('.checkbox-inline').attr("data-error-container"));
+                    } else {
+                        error.insertAfter(element); // for other inputs, just perform default behavior
+                    }
+                },
+
+                highlight: function (element) { // hightlight error inputs
+                   $(element)
+                        .closest('.form-group').addClass('has-error'); // set error class to the control group
+                },
+
+                unhighlight: function (element) { // revert the change done by hightlight
+                    $(element)
+                        .closest('.form-group').removeClass('has-error'); // set error class to the control group
+                },
+
+                success: function (label) {
+                    label
+                        .closest('.form-group').removeClass('has-error'); // set success class to the control group
+                }
+            });
+
+             //apply validation on select2 dropdown value change, this only needed for chosen dropdown integration.
+            $('.select2me', form3).change(function () {
+                form3.validate().element($(this)); //revalidate the chosen dropdown value and show error or success message for the input
+            });
+            
+            $('.validate-form .date-picker .form-control', form3).change(function() {
+                form3.validate().element($(this)); //revalidate the chosen dropdown value and show error or success message for the input 
+            })
+    }
+
+    var handleDatePickers = function () {
+
+        if (jQuery().datepicker) {
+            $('.date-picker').datepicker({
+                rtl: App.isRTL(),
+                autoclose: true
+            });
+            //$('body').removeClass("modal-open"); // fix bug when inline picker is used in modal
+        }
+
+        /* Workaround to restrict daterange past date select: http://stackoverflow.com/questions/11933173/how-to-restrict-the-selectable-date-ranges-in-bootstrap-datepicker */
+    }
+    var handleMonthPickers = function () {
+
+        if (jQuery().datepicker) {
+            $('.month-picker').datepicker({
+                rtl: App.isRTL(),
+                viewMode: "months", 
+                minViewMode: "months",
+                autoclose: true
+            });
+            //$('body').removeClass("modal-open"); // fix bug when inline picker is used in modal
+        }
+
+        /* Workaround to restrict daterange past date select: http://stackoverflow.com/questions/11933173/how-to-restrict-the-selectable-date-ranges-in-bootstrap-datepicker */
+    }
+    
+    var handleTimePickers = function () {
+
+        if (jQuery().timepicker) {
+            $('.timepicker-default').timepicker({
+                autoclose: true,
+                showSeconds: true,
+                minuteStep: 1
+            });
+
+            $('.timepicker-no-seconds').timepicker({
+                autoclose: true,
+                minuteStep: 5
+            });
+
+            $('.timepicker-24').timepicker({
+                autoclose: true,
+                minuteStep: 5,
+                showSeconds: false,
+                showMeridian: false
+            });
+
+            // handle input group button click
+            $('.timepicker').parent('.input-group').on('click', '.input-group-btn', function(e){
+                e.preventDefault();
+                $(this).parent('.input-group').find('.timepicker').timepicker('showWidget');
+            });
+        }
+    }
+    
+    var handleConfirmBox = function() {
+        $('a[data-confirm]').click(function() {
+            var elem = $(this);
+            bootbox.dialog({
+                message: elem.attr('data-confirm'),
+                title: "Confirmation box",
+                buttons: {
+                    confirm: {
+                        label: "Yes",
+                        className: "red",
+                        callback: function() {
+                            window.location.href = elem.attr('href');
+                        }
+                    },
+                    cancel: {
+                        label: "No",
+                        className: "blue"
+                    }
+                }
+            });
+            
+            return false;
+        });
+    }
+    
+    var handleConfirmForm = function() {
+        $('form.confirmation-form').submit(function() {
+            var elem = $(this);
+            if(elem.find('.has-error').length > 0) {
+                return false;
+            }
+            
+            bootbox.dialog({
+                message: 'Are you sure you want to submit this form?',
+                title: "Confirmation box",
+                buttons: {
+                    confirm: {
+                        label: "Yes",
+                        className: "red",
+                        callback: function() {
+                            elem[0].submit();
+                        }
+                    },
+                    cancel: {
+                        label: "No",
+                        className: "blue"
+                    }
+                }
+            });
+            
+            return false;
+        });
+    }
+    
+    return {
+        //main function to initiate the module
+        init: function () {
+            handleDataTables();
+            handleCheckableTable();
+            handleValidation();
+            handleValidation2();
+            handleDatePickers();
+            handleMonthPickers();
+            handleTimePickers();
+            handleConfirmBox();
+            handleConfirmForm();
+        }
+    };
+
+}();
+
+jQuery(document).ready(function() {
+    All.init();
+});
